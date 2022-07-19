8ec38568acf7eb59e782aa9e5c9bb78bca5f5715
peter-wairimu <peter.wairimu@student.moringaschool.com>
2022-07-19 16:27:22 +0300

    shell sort

--- sort.h
+++ sort.h
@@ -31,5 +31,5 @@ void insertion_sort_list(listint_t **list);
 void print_list(const listint_t *list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
-
+void shell_sort(int *array, size_t size);
 #endif
